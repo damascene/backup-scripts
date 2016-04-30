17143f9974271271d05b60ba7f3b2745523d7c0c
Usama Akkad <uahello@gmail.com>
2016-04-30 07:16:25 +0300

    Add license, changed file location.

--- snapshot-busybox-backup.sh
+++ snapshot-busybox-backup.sh
@@ -3,8 +3,17 @@
 ## This version is modifed to work on BusyBox Sh shell
 ## Date command is different in BusyBox from the Bash vrsion
 
-#version 0.1.2
 
+# Copyright 2016 Usama Akkad, Released under GNU General Public License v3.
+
+# BusyBox Snapshot Backup Script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+#Software version 0.1.3
+
+#WARNING - TEST BEFORE YOU USE. A MISTAKE IN RSYNC COULD ERASE YOUR FILES. USE WITH CAUTION.
 #Website Backup Script
 
 #Todays date in ISO-8601 format:
@@ -18,16 +27,16 @@ day1=$(date -I -d @$(( $(date +%s) - (24*60*60) )))
 
 
 #The source directory:
-source="/var/www/htdocs/"
+source="/path/to/source/files/"
 
 #The target directory:
-target="/backup/website/$day0"
+target="/path/to/backup/location/$day0"
 
 #The remote server informaions:
-remote="username@example.org"
+remote="test@example.org"
 
 #The link destination directory:
-link="/backup/website/$day1"
+link="/path/to/backup/location/$day1"
 
 #Rsync options:
 #OPT="-avh -e ssh --delete --link-dest=$LNK"
@@ -35,7 +44,6 @@ link="/backup/website/$day1"
 #Execute the backup
 #Remove --dry-run to activate the command. With --dry-run it will just demonistrate what will happen. 
 rsync -avh -e ssh --dry-run --delete --link-dest="$link" "$remote:$source" "$target"
-
 
 ##optional delete old files
 ##29 days ago in ISO-8601 bash format
@@ -45,7 +53,7 @@ rsync -avh -e ssh --dry-run --delete --link-dest="$link" "$remote:$source" "$tar
 #day29=`date -I -d @$(( $(date +%s) - (29*24*60*60) ))`
 
 #Delete the backup from 29 days ago, if it exists
-#if [ -d /backup/website/$day29 ]
+#if [ -d /path/to/backup/location/$day29 ]
 #then
-#rm /backup/website/$day29
+#rm /path/to/backup/location/$day29
 #fi
